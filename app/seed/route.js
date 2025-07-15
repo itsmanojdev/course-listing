e90252458c107c0c6e1b9e422a2aa85d1314ba22
manoj_edmingle <manoj@edmingle.com>
2025-07-16 01:07:53 +0530

    seeding code refactor

--- app/seed/route.js
+++ app/seed/route.js
@@ -1,46 +1,5 @@
 import sql from "../config/db.js"
 import { tags, courseTagMapper } from "../lib/courseData.js";
-// import { courses, tags, courseTagMapper } from "../lib/courseData.js";
-
-// import { saveImageFromUrl } from "../lib/utils.js";
-// import { createClient } from 'pexels';
-
-// const client = createClient('PwaAeexNNEPfN4PhECaOwwDi39urbzo6VklPZxCwbZ4miPz2SfvwBNDm');
-
-// const seedCourses = async () => {
-//     await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
-//     await sql`
-//         CREATE TABLE IF NOT EXISTS courses (
-//             id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-//             title VARCHAR(255) NOT NULL,
-//             description TEXT NOT NULL,
-//             thumbnail VARCHAR(255),
-//             price INT NOT NULL DEFAULT 0,
-//             discounted_price INT NOT NULL DEFAULT 0 
-//         );
-//     `;
-        
-//     const insertedCourses = await Promise.all(
-//         courses.map(async (course) => {
-//             let courseTag = courseTagMapper.find((ele)=> { return ele.course_id == course.id });
-//             let tag = tags.find((ele) => { return ele.id == courseTag.tag_id});
-            
-//             let thumbnail = "";
-//             // await client.photos.search({ query: tag.tag_name, orientation: "landscape", per_page: 1 }).then(photos => {
-//             //     saveImageFromUrl(photos.photos[0]?.src.original, `/PROJECTS/course-listing/public/thumbnails/${course.id}.jpg`)
-//             //     thumbnail = photos.photos[0]?.src.original;
-//             // });
-
-//             return sql`
-//                 INSERT INTO courses (id, title, description, thumbnail, price, discounted_price)
-//                 VALUES (${course.id}, ${course.title}, ${course.description}, ${thumbnail}, ${course.price}, ${course.discounted_price})
-//                 ON CONFLICT (id) DO NOTHING;
-//             `;
-//         }),
-//     );
-    
-//     return insertedCourses;
-// }
 
 const seedTags = async () => {
     await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
@@ -98,7 +57,6 @@ const seedCourseTagMapper = async () => {
 
 export async function GET() {
     try{
-        // await seedCourses();
         await seedTags();
         await seedCourseTagMapper();
         
